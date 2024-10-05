cb746d0d7dae8eae6406eaaf2acd34cb8d9560fa
Vishnu Goswami <87561982+Vishnu9412@users.noreply.github.com>
2024-10-05 22:21:45 +0530

    build tsting

--- next.config.js
+++ next.config.js
@@ -4,37 +4,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*',
-        port: '',       
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.licdn.com',
-        port: '',       
-      },
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-        port: '',       
-      },
-      {
-        protocol: 'https',
-        hostname: 'live.staticflickr.com',
-        port: '',       
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',       
-      },
-      {
-        protocol: 'https',
-        hostname: 'mms.businesswire.com',
-        port: '',       
+        protocol: "https",
+        hostname: "*",
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
